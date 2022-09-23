9ca1d65ce4f0dae15720e271dd0a8225cd7943c6
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-23 07:02:02 -0700

    Task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,8 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - deletes an entire binary tree
- * @tree: a pointer to the root node of the tree to delete
+ * binary_tree_is_leaf - check if the node is a leaf
+ * @node: a pointer to the node to check
+ *
+ * Return: return 1 if node is a leaf, otherwise 0
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
