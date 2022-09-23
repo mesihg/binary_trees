962154b4ee36e893a0afc5b43298421c572b8524
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-23 13:22:35 -0700

    Task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,12 +11,12 @@ int binary_tree_is_full(const binary_tree_t *tree);
 	if (tree == NULL)
 		return (0);
 
-	if (tree->right != NULL && tree->left != NULL)
+	if (!tree->right && !tree->left)
 		return (1);
+
 	if (tree->right && tree->left)
-	{
 		return (binary_tree_is_full(tree->left) &&
-				binary_tree_is_full(tree->right));
-	}
+			binary_tree_is_full(tree->right));
+
 	return (0);
 }
