d380fc0a3be475b5649ee95f31d190cfebd6cd26
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-23 07:32:10 -0700

    Task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,11 +9,13 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL || funct != NULL)
+	if (tree == NULL || funct == NULL)
+		return;
+
+	else
 	{
 		func(tree->n);
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
 	}
-	return;
 }
