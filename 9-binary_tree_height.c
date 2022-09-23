24dfd3f2ffc21c676d42571cbe11ec7cd1ca1804
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-23 08:08:04 -0700

    Task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,19 +12,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t leftBHeight = 0;
 	size_t rightBHeight = 0;
 
-	if (tree == NULL)
+	if (tree)
 	{
-		return (0);
-	}
-
-	else
-	{
-		leftBHeight = binary_tree_height(tree->left);
-		rightBHeight = binary_tree_height(tree->right);
+		leftBHeight = tree->left ? binary_tree_height(tree->left) : 0;
+		rightBHeight = tree->right ? binary_tree_height(tree->right) : 0;
 
 		if (leftBHeight > rightBHeight)
 			return (leftBHeight + 1);
 		else
 			return (rightBHeight + 1);
 	}
+	return (0);
 }
